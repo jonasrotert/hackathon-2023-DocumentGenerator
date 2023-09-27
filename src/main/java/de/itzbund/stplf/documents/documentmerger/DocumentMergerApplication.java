d8a740dee063a1e9f5f3befa5b7cf31649795e8b
Jonas Rotert <jonas@rotert.online>
2023-09-27 18:13:55 +0200

    Initial commit

---
+++ src/main/java/de/itzbund/stplf/documents/documentmerger/DocumentMergerApplication.java
@@ -0,0 +1,13 @@
+package de.itzbund.stplf.documents.documentmerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentMergerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/itzbund/stplf/documents/documentmerger/DocumentMergerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itzbund.stplf.documents.documentmerger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
