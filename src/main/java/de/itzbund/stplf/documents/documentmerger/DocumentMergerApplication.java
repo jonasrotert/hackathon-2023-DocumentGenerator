b791220d981ee9a2e4f61711571403981eec5a02
Jonas Rotert <jonas@rotert.online>
2023-09-28 09:28:24 +0200

    feat: Added basic CLI support

--- src/main/java/de/itzbund/stplf/documents/documentmerger/DocumentMergerApplication.java
+++ src/main/java/de/itzbund/stplf/documents/documentmerger/DocumentMergerApplication.java
@@ -1,13 +1,25 @@
 package de.itzbund.stplf.documents.documentmerger;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocumentMergerApplication {
+@Log4j2
+public class DocumentMergerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentMergerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		log.info("Starting DocumentMerger");
+
+		for (int i = 0; i < args.length; ++i) {
+			log.info("args[{}]: {}", i, args[i]);
+		}
 	}
 
 }
