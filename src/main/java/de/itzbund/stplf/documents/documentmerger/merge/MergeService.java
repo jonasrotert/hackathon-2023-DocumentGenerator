6b7722a89ed1e2576adac71790d78b2f6717a35f
Jonas Rotert <jonas@rotert.online>
2023-09-28 09:50:57 +0200

    fix: Added Input Parameter for MergeService

--- src/main/java/de/itzbund/stplf/documents/documentmerger/merge/MergeService.java
+++ src/main/java/de/itzbund/stplf/documents/documentmerger/merge/MergeService.java
@@ -1,13 +1,12 @@
 package de.itzbund.stplf.documents.documentmerger.merge;
 
 import de.itzbund.stplf.documents.documentmerger.dto.DocumentDTO;
-import de.itzbund.stplf.documents.documentmerger.dto.DocumentDataDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MergeService {
 
-	public void merge(DocumentDTO) {
+	public void merge(DocumentDTO input) {
 		// Merge Document
 
 	}
