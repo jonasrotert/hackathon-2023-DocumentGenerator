693e8ed1175bd7c9d670053ff2c6e60d5d30b28d
Jonas Rotert <jonas@rotert.online>
2023-09-28 09:40:19 +0200

    feat: Added MergeService

---
+++ src/main/java/de/itzbund/stplf/documents/documentmerger/dto/DocumentDTO.java
@@ -0,0 +1,16 @@
+package de.itzbund.stplf.documents.documentmerger.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class DocumentDTO {
+
+	private DocumentDataDTO data;
+
+	// Attribute for the poi document (input)
+
+	// Attribute for the poi document (output)
+
+}
---
+++ src/main/java/de/itzbund/stplf/documents/documentmerger/merge/MergeService.java
@@ -0,0 +1,15 @@
+package de.itzbund.stplf.documents.documentmerger.merge;
+
+import de.itzbund.stplf.documents.documentmerger.dto.DocumentDTO;
+import de.itzbund.stplf.documents.documentmerger.dto.DocumentDataDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MergeService {
+
+	public void merge(DocumentDTO) {
+		// Merge Document
+
+	}
+
+}
